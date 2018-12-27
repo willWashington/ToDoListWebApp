f6ffcd025b3a1f76f3956a6a8af119ddc0542100
w2 <william.washington@live.com>
2018-12-27 00:41:48 -0500

    Add project files.

---
+++ ToDoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoWebApp.Models;
+
+namespace ToDoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new HomeIndexViewModel();
+            viewModel.ToDoList = new List<string> { "apple", "banana" };
+            return View(viewModel);
+        }
+
+        public IActionResult Add(string toDoItem)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ToDoWebApp/Models/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoWebApp.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<string> ToDoList { get; set; }
+    }
+}
